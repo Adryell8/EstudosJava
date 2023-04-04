6e361f3f9cfde2933db9a87de6ab6c3c7aa959f9
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-04-04 15:35:38 -0300

    Melhoria lançamento de dados

--- src/javaAtividades/AtividadeLancadorDeDado.java
+++ src/javaAtividades/AtividadeLancadorDeDado.java
@@ -8,15 +8,23 @@ public class AtividadeLancadorDeDado {
 	public static void main(String[] args) {
 		Random ran = new Random();
 		Scanner sc = new Scanner(System.in);
+		int quantRolagem = 0;
 		boolean continuar = true;
 
 		while (continuar) {
 			System.out.println("Deseja rolar qual dado (informe o número de lados do dado)? (Sem o número: D6 = 6...)");
 			int Dado = sc.nextInt();
-			if (Dado == 6) {
-				int D6 = ran.nextInt(6) + 1;
-				System.out.println("O resultado da rolagem do dado D6 é: " + D6);
-			} else if (Dado == 8) {
+			String DadoString = String.valueOf(Dado);
+
+			if (DadoString.equalsIgnoreCase("6")) {
+				System.out.println("Informe quantos dados deseja que sejam rolados: ");
+				quantRolagem = sc.nextInt();
+
+				for (int i = 0; i < quantRolagem; i++) {
+					int D6 = ran.nextInt(6) + 1;
+					System.out.println("O resultado da rolagem do dado " + (i + 1) + "°" + " é:" + D6);
+				}
+			} else if (DadoString.equalsIgnoreCase("8")) {
 				int D8 = ran.nextInt(8) + 1;
 				System.out.println("O resultado da rolagem do dado D8 é: " + D8);
 			} else if (Dado == 10) {

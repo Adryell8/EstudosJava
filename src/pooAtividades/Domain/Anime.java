5c026a8d6803994a98781cacc242b7ba92695f7e
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-04-13 23:29:34 -0300

    Entendendo blocos de inicialização em Java

--- src/pooAtividades/Domain/Anime.java
+++ src/pooAtividades/Domain/Anime.java
@@ -3,31 +3,49 @@ package pooAtividades.Domain;
 public class Anime {
 
 	private String nome;
-	private String studio;
-	private int eps;
-	private String genero;
-	
-	public Anime(String nome, int eps, String genero) {
-		this();
+	// private String studio;
+	private int[] eps;
+	// private String genero;
+
+	{
+		eps = new int[100];
+		for (int i = 0; i < eps.length; i++) {
+			eps[i] = i + 1;
+		}
+	}
+
+	public Anime(String nome) {
 		this.nome = nome;
-		this.eps = eps;
-		this.genero = genero;
 	}
-	
-	public Anime(String nome, int eps, String genero, String studio) {
-		this(nome, eps, genero);
-		this.studio = studio;
-	}
-	
+
 	public Anime() {
-		
+		for (int ep : this.eps) {
+			System.out.println(ep + " ");
+		}
 	}
-	
-	public void imprime() {
-		System.out.println(this.nome);
-		System.out.println(this.eps);
-		System.out.println(this.genero);
-		System.out.println(this.studio);
+
+	/**
+	 * public Anime(String nome, int eps, String genero) { this(); this.nome = nome;
+	 * this.eps = eps; this.genero = genero; }
+	 * 
+	 * public Anime(String nome, int eps, String genero, String studio) { this(nome,
+	 * eps, genero); this.studio = studio; }
+	 * 
+	 * public Anime() {
+	 * 
+	 * }
+	 * 
+	 * public void imprime() { System.out.println(this.nome);
+	 * System.out.println(this.eps); System.out.println(this.genero);
+	 * System.out.println(this.studio); }
+	 */
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int[] getEps() {
+		return eps;
 	}
 
 }
--- src/pooAtividades/Test/AnimeTeste.java
+++ src/pooAtividades/Test/AnimeTeste.java
@@ -6,8 +6,10 @@ import pooAtividades.Domain.Anime;
 public class AnimeTeste {
 
 	public static void main(String[] args) {
-		Anime anime = new Anime("one peace",1300,"romance", "Matta");
-		anime.imprime();
+		Anime anime = new Anime();
+		for(int ep: anime.getEps()) {
+			System.out.println(ep + " ");
+		}
 	}
 
 }

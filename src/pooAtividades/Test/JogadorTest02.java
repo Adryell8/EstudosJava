346c7fa2ff386303aa7a00da3cd331d7d8cd7a52
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-04-16 19:33:53 -0300

    JogadorTest02 agora com interação com usuário

--- src/pooAtividades/Test/JogadorTest02.java
+++ src/pooAtividades/Test/JogadorTest02.java
@@ -1,16 +1,20 @@
 package pooAtividades.Test;
 
+import java.util.Scanner;
+
 import pooAtividades.Domain.Jogador;
 import pooAtividades.Domain.Time;
 
 public class JogadorTest02 {
 
 	public static void main(String[] args) {
-
-		Jogador jogador1 = new Jogador("Ney");
-		Time time = new Time("PSG");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Informe o nome do jogador: ");
+		Jogador jogador1 = new Jogador(sc.nextLine());
+		System.out.println("Informe o time do jogador: ");
+		Time time = new Time(sc.nextLine());
 		
-		jogador1.setTime(time);
+		jogador1.setTime(time); // Time está sendo "setado" em jogador por meio de jogador1
 		jogador1.imprime();
 		
 	}

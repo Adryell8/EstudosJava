823005a7f2f5ba680be3675b2e8ea1e3673c67ca
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-04-15 23:39:23 -0300

    Associação em Java pt.01 - Arrays com objetos

---
+++ src/pooAtividades/Test/JogadorTest.java
@@ -0,0 +1,19 @@
+package pooAtividades.Test;
+
+import pooAtividades.Domain.Jogador;
+
+public class JogadorTest {
+
+	public static void main(String[] args) {
+		Jogador jogador1 = new Jogador("Pelé");
+		Jogador jogador2 = new Jogador("Ney");
+		Jogador jogador3 = new Jogador("CR7");
+		Jogador[] jogadores = {jogador1, jogador2, jogador3};
+		
+		for(Jogador jogador : jogadores) {
+			jogador.imprime();
+		}
+		
+	}
+
+}

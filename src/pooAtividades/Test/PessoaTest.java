af07d3b0c0fc6d90e15e711afe1ca9de61a4da07
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-05-08 19:46:04 -0300

    Atividade Pessoa01 Test

--- src/pooAtividades/Test/PessoaTest.java
+++ src/pooAtividades/Test/PessoaTest.java
@@ -1,10 +1,23 @@
 package pooAtividades.Test;
 
-public class PessoaTest {
+import javax.swing.JOptionPane;
+
+import pooAtividades.Domain.Pessoa01;
+
+public class PessoaTest{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Pessoa01 pessoa = new Pessoa01();
+		String nome = JOptionPane.showInputDialog("Informe o seu nome");
+		pessoa.setNome(nome);
+		String sobreNome = JOptionPane.showInputDialog("Informe o seu sobrenome");
+		pessoa.setSobreNome(sobreNome);
+		int AnoDeNascimento = Integer.parseInt((JOptionPane.showInputDialog("Informe a sua data de nascimento")));
+		pessoa.setAnoDeNascimento(AnoDeNascimento);
+
+		JOptionPane.showMessageDialog(null, pessoa.JuntaNome() + ". Ano nascimento:" + pessoa.getAnoDeNascimento());
+		JOptionPane.showMessageDialog(null, "Você tem: " + pessoa.QtdAnos());
 	}
 
 }

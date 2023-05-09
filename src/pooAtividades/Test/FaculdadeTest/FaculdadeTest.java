a0b7f5fb2031094b1dbc08721f3a745c47b9d4f9
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-05-09 19:04:36 -0300

    Projeto Faculdade - Classe FaculdadeTest feita

--- src/pooAtividades/Test/FaculdadeTest/FaculdadeTest.java
+++ src/pooAtividades/Test/FaculdadeTest/FaculdadeTest.java
@@ -1,10 +1,44 @@
 package pooAtividades.Test.FaculdadeTest;
 
+import java.util.Scanner;
+import pooAtividades.Domain.FaculdadeDomain.Aluno;
+import pooAtividades.Domain.FaculdadeDomain.Disciplina;
+
 public class FaculdadeTest {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		Aluno al = new Aluno();
+		System.out.println("Informe o nome do Aluno: ");
+		al.setNome(input.nextLine());
 
+		System.out.println("Informe a idade do Aluno: ");
+		al.setIdade(input.nextInt());
+
+		System.out.println("Informe a faculdade do aluno: ");
+		input.nextLine();
+		al.setFaculdade(input.nextLine());
+
+		System.out.println("Informe a quantidade de disciplinas: ");
+		int qtdDisciplinas = input.nextInt();
+
+		for (int i = 1; i <= qtdDisciplinas; i++) {
+			input.nextLine();
+
+			System.out.println("Informe a disciplina " + i + ": ");
+			String disc = input.nextLine();
+
+			System.out.println("Informe a nota " + i + ": ");
+			double nota = input.nextDouble();
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(disc);
+			disciplina.setNota(nota);
+
+			al.getDisciplinas().add(disciplina);
+		}
+		// System.out.println("Média do aluno: " + al.calculaMedia());
+		// System.out.println("Situação do aluno:" + al.SituacaoAluno());
+		System.out.println(al);
+		input.close();
 	}
 
 }

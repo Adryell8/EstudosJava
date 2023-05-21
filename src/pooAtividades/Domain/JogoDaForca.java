92510da0a84af301dbf924379b6499684b2cbf09
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-05-21 19:39:49 -0300

    Criação método verificaPalavra Classe Forca - Projeto Forca

--- src/pooAtividades/Domain/JogoDaForca.java
+++ src/pooAtividades/Domain/JogoDaForca.java
@@ -1,5 +1,27 @@
 package pooAtividades.Domain;
 
-public class JogoDaForca {
+import java.util.Random;
 
+public class JogoDaForca {
+	private static final String[] PALAVRAS = {"ABACAXI", "BANANA", "MELANCIA", "LARANJA", "MORANGO"};
+	
+	public String selecionaPalavra() {
+		Random ran = new Random();
+		int indicePalavra = ran.nextInt(PALAVRAS.length);
+		return PALAVRAS[indicePalavra];
+	}
+	
+	private boolean verificaPalavra(String palavra, char[] palavraParcial, char letra) {
+		boolean letraEncontrada = false;
+		
+		for (int i = 0; i < palavra.length(); i++) {
+			if(palavra.charAt(i) == letra) {
+				palavraParcial[i] = letra;
+				letraEncontrada = true;
+			}
+		}
+		return letraEncontrada;
+
+	}
+	
 }

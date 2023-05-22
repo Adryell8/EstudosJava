2cd11934dec482ddcdc45150aa171a12beb3bbea
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-05-21 21:23:51 -0300

    Classe Forca executável completa pt.01 - Projeto Forca

--- src/pooAtividades/Domain/JogoDaForca.java
+++ src/pooAtividades/Domain/JogoDaForca.java
@@ -3,19 +3,20 @@ package pooAtividades.Domain;
 import java.util.Random;
 
 public class JogoDaForca {
-	private static final String[] PALAVRAS = {"ABACAXI", "BANANA", "MELANCIA", "LARANJA", "MORANGO"};
-	
+	private final String[] PALAVRAS = { "ABACAXI", "BANANA", "MELANCIA", "LARANJA", "MORANGO" };
+	private final int MAX_TENTATIVAS = 10;
+
 	public String selecionaPalavra() {
 		Random ran = new Random();
 		int indicePalavra = ran.nextInt(PALAVRAS.length);
 		return PALAVRAS[indicePalavra];
 	}
-	
-	private boolean verificaPalavra(String palavra, char[] palavraParcial, char letra) {
+
+	public boolean verificaPalavra(String palavra, char[] palavraParcial, char letra) {
 		boolean letraEncontrada = false;
-		
+
 		for (int i = 0; i < palavra.length(); i++) {
-			if(palavra.charAt(i) == letra) {
+			if (palavra.charAt(i) == letra) {
 				palavraParcial[i] = letra;
 				letraEncontrada = true;
 			}
@@ -23,5 +24,13 @@ public class JogoDaForca {
 		return letraEncontrada;
 
 	}
-	
+
+	public String[] getPalavras() {
+		return PALAVRAS;
+	}
+
+	public int getMaxTentativas() {
+		return MAX_TENTATIVAS;
+	}
+
 }
--- src/pooAtividades/Test/Foca.java
+++ src/pooAtividades/Test/Foca.java
@@ -1,8 +1,43 @@
 package pooAtividades.Test;
 
+import java.util.Scanner;
+
+import pooAtividades.Domain.JogoDaForca;
+
 public class Foca {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		JogoDaForca forca = new JogoDaForca();
+		
+		String palavra = forca.selecionaPalavra();
+		char[] palavraParcial = new char[palavra.length()];
+		
+		int tentativa = 0;
+		
+		for (int i = 0; i < palavraParcial.length; i++) {
+			palavraParcial[i] = '-'; 
+		}
+		
+		while(tentativa < forca.getMaxTentativas()) {
+			System.out.println("\nA palavra: "+String.valueOf(palavraParcial));
+			System.out.println("Digite uma letra:");
+			char letra = input.nextLine().toUpperCase().charAt(0);
+			
+			if(forca.verificaPalavra(palavra, palavraParcial, letra)) {
+				if(String.valueOf(palavraParcial).equals(palavra)) {
+					System.out.println("Você acertou! A palavra era: "+palavra+"!");
+					break;
+				}
+			}else {
+				tentativa++;
+				System.out.println("A letra incorreta. Você possui: "+ (forca.getMaxTentativas() - tentativa)+" de tentivas");
+			}
+			
+			if(tentativa == forca.getMaxTentativas()) {
+				System.out.println("Você atingou o número máximo de tetnaivas, tente novamente.\nA palavra correta era: "+palavra);
+			}
+		}
 		
 	}
 

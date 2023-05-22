fbf1bfb2062ff8a3b3508a8a9862565c7146a09a
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-05-22 18:08:28 -0300

    Classe Pessoa e regras das classes abstratas - Estudo Classes abstratas

--- src/pooAtividades/Domain/EnumDomain/Funcionario.java
+++ src/pooAtividades/Domain/EnumDomain/Funcionario.java
@@ -1,17 +1,20 @@
 package pooAtividades.Domain.EnumDomain;
 
-public abstract class Funcionario {
+public abstract class Funcionario extends Pessoa{
 	protected String nome;
 	protected double salario;
 	
+	@Override
+	public void imprime() {
+		
+	}
+
 	public Funcionario(String nome, double salario) {
 		this.nome = nome;
 		this.salario = salario;
 		calculaBonus();
 	} // Um método só pode ser abstrato quando a classe também é abstrata, mas uma classe abstrata pode ter métodos concretos e abstratos
 	
-	public abstract void calculaBonus();
-	
-	
+	public abstract void calculaBonus(); // métodos abstratos não tem corpo e sempre é preciso prover uma "subclasse" para ele
 	
 }
---
+++ src/pooAtividades/Domain/EnumDomain/Pessoa.java
@@ -0,0 +1,5 @@
+package pooAtividades.Domain.EnumDomain;
+
+public abstract class Pessoa {
+	public abstract void imprime();
+}	

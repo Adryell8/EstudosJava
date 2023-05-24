6bfc224bdd36132c2e7b3704b3e4a9944679aa53
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-05-23 21:02:40 -0300

    Métodos concretos - Estudo Interfaces Java

--- src/pooAtividades/Domain/InterfacesDomain/DataLoader.java
+++ src/pooAtividades/Domain/InterfacesDomain/DataLoader.java
@@ -2,4 +2,7 @@ package pooAtividades.Domain.InterfacesDomain;
 
 public interface DataLoader {
 	void load();
+	default void checkPermission() {
+		System.out.println("Fazendo checagem de permissões");
+	}
 }
